e4a887927eb515c22156b652faa5f2fd8a086406
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-12-05 22:53:06 +1100

    Initial commit

---
+++ src/routes/pop-alert/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import svelteWelcome from "$lib/images/svelte-welcome.png"
+
+  const attack = '" onload="alert(\'uhoh\')" data-nothing="not important'
+</script>
+
+<img src={svelteWelcome} style:--css-variable={attack} alt="dangerous attributes ssr">
+
+<p>
+  Did you see an alert popup?
+</p>
